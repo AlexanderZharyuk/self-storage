2c45c0a6e3fd0bed0f75e212de5a32e82c1fdffa
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-06-30 14:39:04 +0300

    Добавил новый файл для общих функций и функцию is_new_user

---
+++ general_functions.py
@@ -0,0 +1,10 @@
+import json
+
+
+def is_new_user(user_id: str) -> bool:
+    """Функция возвращает True или False в зависимости от того - есть ли пользователь в базе данных"""
+    with open('json_files/users_order.json', 'r') as file:
+        users = json.load(file)
+
+    users_ids = [user['user_id'] for user in users if user['user_id'] == user_id]
+    return not bool(users_ids)
