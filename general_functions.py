ca8c6d6d1e86873cb8a11de37b4df76dee3283f3
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-07-01 15:39:45 +0300

    Добавлены функции по получению данных о заказах пользователя

--- general_functions.py
+++ general_functions.py
@@ -8,3 +8,18 @@ def is_new_user(user_id: int) -> bool:
 
     users_ids = [user['user_id'] for user in users if user['user_id'] == user_id]
     return not bool(users_ids)
+
+
+def get_orders(user_id: int) -> list:
+    """Функция для получения списка заказов пользователя"""
+    with open('json_files/users_order.json', 'r') as file:
+        users = json.load(file)
+    user_orders = [user['orders'] for user in users if user['user_id'] == user_id][0]
+    return user_orders
+
+
+def get_orders_ids(user_id: int) -> list:
+    """Функция получения айди всех заказов пользователя"""
+    orders = get_orders(user_id)
+    user_orders_id = [order['order_id'] for order in orders]
+    return user_orders_id
