0b0fb6cb330966d141f15083e21636697efd2d84
leenythebear <Leeny@inbox.ru>
2022-07-01 21:09:37 +0700

    make func to create message

--- general_functions.py
+++ general_functions.py
@@ -14,12 +14,24 @@ def get_orders(user_id: int) -> list:
     """Функция для получения списка заказов пользователя"""
     with open('json_files/users_order.json', 'r') as file:
         users = json.load(file)
-    user_orders = [user['orders'] for user in users if user['user_id'] == user_id][0]
+    user_orders = [user['orders'] for user in users if user['user_id'] == user_id]
     return user_orders
 
 
-def get_orders_ids(user_id: int) -> list:
+def create_info_message(order_id, user_id):
+    orders = get_orders(user_id)[0]
+    for order in orders:
+        if order_id == order['order_id']:
+            warehouse_id = order['warehouse_id']
+            start_date = order['start_date']
+            end_date = order['end_date']
+            qr_code = order['qr_code']
+            message = f'Ваш бокс # {order_id} находится на складе {warehouse_id}. Срок хранения {start_date} - {end_date}. QR-code: {qr_code}.'
+    return message
+
+
+def get_orders_ids(user_id: int)-> list:
     """Функция получения айди всех заказов пользователя"""
-    orders = get_orders(user_id)
+    orders = get_orders(user_id)[0]
     user_orders_id = [order['order_id'] for order in orders]
     return user_orders_id
