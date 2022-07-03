a07ff4763e237c7515d3e48b9c0ac86c4eaaee80
leenythebear <Leeny@inbox.ru>
2022-07-03 23:00:36 +0700

    add inline button for qr-code

--- bot.py
+++ bot.py
@@ -158,6 +158,11 @@ def get_box_info(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
     info_message = create_info_message(order_id, user_id)
     update.message.reply_text(info_message, reply_markup=markup)
+
+    button = InlineKeyboardButton("QR", callback_data=order_id)
+    reply_markup_qr = InlineKeyboardMarkup([[button]])
+    update.message.reply_text('Нажмите, чтобы получить QR-code', reply_markup=reply_markup_qr)
+
     return ORDERS
 
 
